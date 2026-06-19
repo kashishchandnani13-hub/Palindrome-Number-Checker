9ff286451caf7fe1756c6ad892af11ecd543c7d8
Kashish Chandnani <kashishchandnani13@gmail.com>
2026-06-19 23:54:27 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 void Palindrome(int num){
+    if(num < 0){
+        printf("%d is not a palindrome number\n", n);
+    }
     int  n=num , rev=0, rem;
         
-        while(num != 0){
-            rem = num % 10;
-            rev = rev * 10 + rem;
-            num = num / 10;
-        }
-        if (n == rev){
-            printf("%d is a palindrome number\n", n);
-        }
-        else{
-            printf("%d is not a palindrome number\n",n);
-        }
+    while(num != 0){
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+    if (n == rev){
+        printf("%d is a palindrome number\n", n);
+    }
+    else{
+        printf("%d is not a palindrome number\n",n);
+    }
 }
 
 int main(){
